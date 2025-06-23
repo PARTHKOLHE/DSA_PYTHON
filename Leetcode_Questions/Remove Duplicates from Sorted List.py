50f0123943352a2101bb88d5198212b4d8373418
Parth Kolhe <parthkolhe456@gmail.com>
2025-06-23 22:58:00 +0530

    modiying remove dupicate again

--- Leetcode_Questions/Remove Duplicates from Sorted List.py
+++ Leetcode_Questions/Remove Duplicates from Sorted List.py
@@ -1,8 +1,9 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+from typing import Optional
+Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         current = head
