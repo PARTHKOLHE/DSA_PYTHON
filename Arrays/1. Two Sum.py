0c6253470ba8d9f0d41ca936ec2b6cad125de130
Parth Kolhe <parthkolhe456@gmail.com>
2025-07-03 23:38:56 +0530

    starting arrays- two sums

---
+++ Arrays/1. Two Sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+## Brute force 
+        # for i in range(len(nums)):
+        #     n = nums[i]
+            
+        #     for j in range(i+1, len(nums)):
+        #         w = nums[j]
+        #         if n + w == target:
+        #             return [i,j]
+                
+## Optimal
+        prevArr={}
+        for i,n in enumerate(nums):
+            diff = target - n
+            if diff in prevArr:
+                return [prevArr[diff],i]
+            prevArr[i] = n
+        return 
+            
+            
+            
+        
+        
