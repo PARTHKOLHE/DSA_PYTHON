9e34856035a7493dc2757d5812bba4ab3a7ad8bf
Parth Kolhe <parthkolhe456@gmail.com>
2025-07-24 00:11:40 +0530

    greedy algo

--- Greedy Algorithm/Minimum Platforms.py
+++ Greedy Algorithm/Minimum Platforms.py
@@ -1,25 +1,19 @@
 #User function Template for python3
-
-class Solution:   
+import heapq
+class Solution:    
     def minimumPlatform(self,arr,dep):
-        nums = []
-        for a,d in zip(arr,dep):
-            nums.append([a,d])
-            
-        nums.sort(key = lambda x: (x[1],x[0]))
-        count = 0
-        min_val = []
+        n = len(arr)
+        trains = sorted(zip(arr, dep), key = lambda x: x[0])
+        heap = []
+        heapq.heappush(heap,trains[0][1])
         
-        for i in range(len(nums)):
-            for j in range(len(min_val)):
-                if nums[i][0] > min_val[j]:
-                    min_val.pop(j)
-                    min_val.append(nums[i][1])
-                    
-                else:
-                    count += 1
-                    min_val.append(nums[i][1])
-        return count
+        for i in range(1, n):
+            if trains[i][0] > heap[0]:
+                heapq.heappop(heap)
+            
+            heapq.heappush(heap, trains[i][1])
+        
+        return len(heap)
                 
                 
                 
