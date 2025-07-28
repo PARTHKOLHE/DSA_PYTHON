b38c0078c759c93d4b6837ddc6a0d935e6aec4f1
Parth Kolhe <parthkolhe456@gmail.com>
2025-07-28 23:28:08 +0530

    recurssion

---
+++ Recusrsion/Subset Sums.py
@@ -0,0 +1,9 @@
+class Solution:
+    def subsetSums(self, arr):
+        output_arr = [0]
+        
+        for i in arr:
+            output_arr.append(arr[i])
+            
+            
+        
